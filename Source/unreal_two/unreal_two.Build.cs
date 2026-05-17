f90692cfc912ebf435c420c7812e0fbc7fee3b1c
Nist0n <148474331+Nist0n@users.noreply.github.com>
2026-05-17 19:52:30 +0300

    Первый рабочий квест

--- Source/unreal_two/unreal_two.Build.cs
+++ Source/unreal_two/unreal_two.Build.cs
@@ -8,7 +8,7 @@ public class unreal_two : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "QuestSystem" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
